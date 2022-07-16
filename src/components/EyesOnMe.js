708191ca5c73e75b8f2a238e382c81ecfba6cd0e
Mwenje <mwenje24@gmail.com>
2022-07-16 11:37:46 +0300

    created Keypad and EyesonMe components and added onchange, onblur and onfocus event listeners

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+function onFocusEvent(){
+    console.log('Good!')
+  }
+
+function onBlurEvent(){
+console.log('Hey! Eyes on me!')
+}
+
+export default function EyesOnMe() {
+  return (
+    <div>
+      <button onFocus={onFocusEvent} onBlur={onBlurEvent}>Eyes on me</button>
+    </div>
+  )
+}
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React from 'react'
+
+function onChangeEvent(){
+  console.log('Entering password...')
+}
+
+function Keypad() {
+  return (
+    <div>
+      <input name='password' type='password' onChange={onChangeEvent}/>
+    </div>
+  )
+}
+
+export default Keypad
